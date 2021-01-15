5cd05ae15875c54e2ff7da1af8dbcac3f329a861
vincentc48 <vincent.cai48@gmail.com>
2021-01-15 17:57:16 -0500

    add the mission page, preliminary styles, data fetched from firebase

--- client/src/components/Mission.js
+++ client/src/components/Mission.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { firestore } from "../config/firebase";
 
 class Mission extends React.Component {
   constructor() {
     super();
+    this.state = {
+      mainText: "",
+      secondaryText: "",
+    };
+  }
+
+  componentDidMount() {
+    firestore
+      .collection("settings")
+      .doc("mission")
+      .get()
+      .then((doc) => {
+        this.setState({
+          mainText: doc.data().main,
+          secondaryText: doc.data().secondary,
+        });
+      });
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div id="mission-container">
+        <h2>Mission</h2>
+        <section id="mission-main">{this.state.mainText}</section>
+
+        <section id="mission-secondary">{this.state.secondaryText}</section>
+      </div>
+    );
   }
 }
 
